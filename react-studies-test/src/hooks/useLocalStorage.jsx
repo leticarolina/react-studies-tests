b6b7992179582f0a7a2006dd4394f894ebad3f26
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-02-27 00:34:02 -0800

    useLocalStorage exercise

--- react-studies-test/src/hooks/useLocalStorage.jsx
+++ react-studies-test/src/hooks/useLocalStorage.jsx
@@ -24,3 +24,62 @@ export function useLocalStorage(key, initialValue) {
 
   return [value, setValue];
 }
+
+//code from main file
+//code from main file
+//code from main file
+export default function Home() {
+  const [firstName, setFirstName] = useLocalStorage("FIRST_NAME", "");
+
+  // Bonus:
+  // const [lastName, setLastName] = useLocalStorage("LAST_NAME", () => {
+  //   return "Default"
+  // })
+
+  // Bonus:
+  const [hobbies, setHobbies] = useLocalStorage("HOBBIES", [
+    "Programming",
+    "Weight Lifting",
+  ]);
+
+  return (
+    <>
+      <div>
+        <label>First Name</label>
+        <input
+          className="bg-blue-400"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+
+      {/* Bonus: */}
+      {/* <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>Last Name</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+      </div> */}
+
+      {/* Bonus: */}
+      <div>{hobbies.join(", ")}</div>
+      <button
+        onClick={() =>
+          setHobbies((currentHobbies) => [...currentHobbies, "New Hobby"])
+        }
+      >
+        Add Hobby
+      </button>
+    </>
+  );
+}
