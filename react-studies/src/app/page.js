56f66ce96a8335ee17b2dcc9c2b223a87b42113f
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-06-10 21:30:54 -0700

    portals review 1

--- react-studies/src/app/page.js
+++ react-studies/src/app/page.js
@@ -1,10 +1,35 @@
+"use client";
+import { useState } from "react";
 import { CustomModal } from "@/components/CustomModal";
+import { PortalsAlertMessage } from "@/components/PortalsAlertMessage";
 import Image from "next/image";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <h1>App Content</h1>;<button>Click to open portal</button>
+    <div
+      style={{
+        backgroundColor: "pink",
+        position: "relative",
+        marginTop: "100px",
+      }}
+    >
+      <h1>App Content</h1>
+      <button
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Click to open Modal as a Portal
+      </button>
+      <PortalsAlertMessage
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        Alert Message
+      </PortalsAlertMessage>
     </div>
   );
 }
--- react-studies/src/components/PortalsAlertMessage.jsx
+++ react-studies/src/components/PortalsAlertMessage.jsx
@@ -1,1 +1,32 @@
 //----------- React Portals (recoding previous study component file was lost)
+//portals are basically used to rendwer an element outside of the hierarchy of the DOM.
+import { useState } from "react";
+const { createPortal } = require("react-dom");
+
+//to create a portal need to use the ReactDOM.createPortal function , if you imported the portal from react-dom can just declare the function as CreatePortal
+//This function takes two arguments: the JSX you want to render and the DOM node where you want to render it.
+
+export function PortalsAlertMessage({ children, onClose, isOpen }) {
+  //children is gonna be what is being rendered
+  //onclose when modal is close, isOpen when true
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div
+      onClick={onClose}
+      style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        background: "lightBlue",
+        padding: "20px",
+        borderRadius: "5px",
+        zIndex: 9999,
+      }}
+    >
+      {children}
+    </div>,
+    document.querySelector("#portal-container")
+  );
+}
