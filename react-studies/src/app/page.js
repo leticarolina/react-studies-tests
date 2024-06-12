fdd86c9bc3b4cda06de4dbaff882ec25cbd8ec39
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-06-11 21:29:36 -0700

    review 2 of partals

--- react-studies/src/app/page.js
+++ react-studies/src/app/page.js
@@ -8,27 +8,33 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div
+      // position: relative on main div container to act as the reference for the modal's absolute position.
       style={{
         backgroundColor: "pink",
         position: "relative",
         marginTop: "100px",
       }}
     >
+      {/* Rendering portal within a dedicated container in the main component: */}
+      <div id="portal-container"></div>
       <h1>App Content</h1>
       <button
+        // button change isOpen state, which controls the visibility of the modal
         onClick={() => {
           setIsOpen(true);
         }}
       >
         Click to open Modal as a Portal
       </button>
+
       <PortalsAlertMessage
         isOpen={isOpen}
         onClose={() => {
           setIsOpen(false);
         }}
       >
-        Alert Message
+        Alert Message this is the children prop on PortalsAlertMessage
+        component.
       </PortalsAlertMessage>
     </div>
   );
--- react-studies/src/components/PortalsAlertMessage.jsx
+++ react-studies/src/components/PortalsAlertMessage.jsx
@@ -1,22 +1,33 @@
 //----------- React Portals (recoding previous study component file was lost)
 //portals are basically used to rendwer an element outside of the hierarchy of the DOM.
-import { useState } from "react";
-const { createPortal } = require("react-dom");
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 //to create a portal need to use the ReactDOM.createPortal function , if you imported the portal from react-dom can just declare the function as CreatePortal
-//This function takes two arguments: the JSX you want to render and the DOM node where you want to render it.
+//This function createPortal()takes two arguments:1.the JSX you want to render, 2 the DOM node where you want to render it.
+
+//this is the whole component for portal, typically takes three props
+// children: The content that will be displayed inside the modal.
+// onClose: A function that will be called to close the modal.
+// isOpen: A boolean value that determines whether the modal is open or not.
 
 export function PortalsAlertMessage({ children, onClose, isOpen }) {
-  //children is gonna be what is being rendered
-  //onclose when modal is close, isOpen when true
+  //in this example I am targetting a html element on the main pafe to render my portal
+  //the useEffect hook is used to set the container after the component mounts. This ensures that document.querySelector("#portal-container") doesnt return null and runs only once and avoids unnecessary re-renders.
+  const [container, setContainer] = useState(null);
+  useEffect(() => {
+    setContainer(document.querySelector("#portal-container"));
+  }, []);
+
+  // The PortalsAlertMessage component is conditionally rendered based on if the isOpen state is true
   if (!isOpen) return null;
 
   return createPortal(
     <div
       onClick={onClose}
       style={{
-        position: "fixed",
-        top: "50%",
+        position: "absolute",
+        top: "200px",
         left: "50%",
         transform: "translate(-50%, -50%)",
         background: "lightBlue",
@@ -27,6 +38,6 @@ export function PortalsAlertMessage({ children, onClose, isOpen }) {
     >
       {children}
     </div>,
-    document.querySelector("#portal-container")
+    container
   );
 }
